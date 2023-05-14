aec3a79455376c6dd3df0e9c1ac84a1eb524fa28
Aviral Srivastava <81246801+meaviral17@users.noreply.github.com>
2023-05-14 10:38:47 +0530

    Update DistanceMachine.h

--- src/shogun/machine/DistanceMachine.h
+++ src/shogun/machine/DistanceMachine.h
@@ -12,20 +12,18 @@
 
 #include <shogun/lib/common.h>
 #include <shogun/machine/Machine.h>
+#include <memory>
 
-
-namespace shogun
-{
+namespace shogun {
 	class Distance;
 	class Features;
 	class MulticlassLabels;
 
-/** @brief A generic DistanceMachine interface.
- *
- * A distance machine is based on a a-priori choosen distance.
- */
-class DistanceMachine : public Machine
-{
+	/** @brief A generic DistanceMachine interface.
+	 *
+	 * A distance machine is based on a a-priori chosen distance.
+	 */
+	class DistanceMachine : public Machine {
 	public:
 		/** default constructor */
 		DistanceMachine();
@@ -33,71 +31,31 @@ class DistanceMachine : public Machine
 		/** destructor */
 		~DistanceMachine() override;
 
-		/** set distance
-		 *
-		 * @param d distance to set
-		 */
+		/** set distance */
 		void set_distance(std::shared_ptr<Distance> d);
 
-		/** get distance
-		 *
-		 * @return distance
-		 */
+		/** get distance */
 		std::shared_ptr<Distance> get_distance() const;
 
-		/**
-		 * get distance functions for lhs feature vectors
-		 * going from a1 to a2 and rhs feature vector b
-		 *
-		 * @param result array of distance values
-		 * @param idx_a1 first feature vector a1 at idx_a1
-		 * @param idx_a2 last feature vector a2 at idx_a2
-		 * @param idx_b feature vector b at idx_b
-		 */
-		void distances_lhs(SGVector<float64_t>& result, int32_t idx_a1, int32_t idx_a2, int32_t idx_b);
-
-		/**
-		 * get distance functions for rhs feature vectors
-		 * going from b1 to b2 and lhs feature vector a
-		 *
-		 * @param result array of distance values
-		 * @param idx_b1 first feature vector a1 at idx_b1
-		 * @param idx_b2 last feature vector a2 at idx_b2
-		 * @param idx_a feature vector a at idx_a
-		 */
-		void distances_rhs(SGVector<float64_t>& result, int32_t idx_b1, int32_t idx_b2, int32_t idx_a);
-
-		/** Returns the name of the SGSerializable instance.  It MUST BE
-		 *  the CLASS NAME without the prefixed `C'.
-		 *
-		 * @return name of the SGSerializable
-		 */
-		const char* get_name() const override { return "DistanceMachine"; }
-
-		/** Classify all provided features.
-		 * Cluster index with smallest distance to to be classified element is
-		 * returned
-		 *
-		 * @param data (test)data to be classified
-		 * @return classified labels
-		 */
-		std::shared_ptr<MulticlassLabels> apply_multiclass(std::shared_ptr<Features> data=NULL) override;
-
-		/** Apply machine to one example.
-		 * Cluster index with smallest distance to to be classified element is
-		 * returned
-		 *
-		 * @param num which example to apply machine to
-		 * @return cluster label nearest to example
-		 */
-		float64_t apply_one(int32_t num) override;
-
-	private:
-		void init();
+		/** apply to multiclass labels */
+		std::shared_ptr<MulticlassLabels> apply_multiclass(std::shared_ptr<Features> data);
 
 	protected:
-		/** the distance */
+		/** initialize machine before training (train or apply) */
+		void init() override;
+
+		/** calculate distances to lhs */
+		void distances_lhs(SGVector<float64_t>& result, index_t idx_a1, index_t idx_a2, index_t idx_b);
+
+		/** calculate distances to rhs */
+		void distances_rhs(SGVector<float64_t>& result, index_t idx_b1, index_t idx_b2, index_t idx_a);
+
+		/** apply to one vector */
+		float64_t apply_one(int32_t num);
+
+	private:
+		/** distance to use */
 		std::shared_ptr<Distance> distance;
-};
+	};
 }
-#endif
+#endif // _DISTANCE_MACHINE_H__
